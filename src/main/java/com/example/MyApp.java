38924a961072de34a007998349e93e90db8afbad
Michał Słota <irreftw@gmail.com>
2022-01-12 18:05:07 +0100

    zmiana app config

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -2,9 +2,8 @@ package com.example;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"guru.springframework.sfgdi", "com.springframework.pets"})
+
 public class MyApp {
 
     public static void main(String[] args) {
@@ -14,7 +13,7 @@ public class MyApp {
         MessageService messageServiceNumberTwo = applicationContext.getBean("messageService", MessageService.class);
 
         System.out.println(messageService.getMessage());
-        System.out.println("Hascode of message service = " + messageService.hashCode() +
+        System.out.println("Hashcode of message service = " + messageService.hashCode() +
                 "\nHashcode of messageServiceNumberTwo = " + messageServiceNumberTwo.hashCode() +
                 "\nAre they the same object? " + messageService.equals(messageServiceNumberTwo));
 
