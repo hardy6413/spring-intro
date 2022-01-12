3967af7793eb4325a441c7452b9b6998b54baa7f
Michał Słota <irreftw@gmail.com>
2022-01-12 17:57:40 +0100

    java commit

--- src/main/java/com/example/MyApp.java
+++ src/main/java/com/example/MyApp.java
@@ -1,11 +1,14 @@
 package com.example;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"guru.springframework.sfgdi", "com.springframework.pets"})
 public class MyApp {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MyAppConfig.class);
 
         MessageService messageService = applicationContext.getBean("messageService", MessageService.class);
         MessageService messageServiceNumberTwo = applicationContext.getBean("messageService", MessageService.class);
@@ -15,6 +18,5 @@ public class MyApp {
                 "\nHashcode of messageServiceNumberTwo = " + messageServiceNumberTwo.hashCode() +
                 "\nAre they the same object? " + messageService.equals(messageServiceNumberTwo));
 
-        applicationContext.close();
     }
 }
